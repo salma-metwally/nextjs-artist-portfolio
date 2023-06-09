7d9fd10372c053a18eb515fd884c63027032a6af
salma-metwally <136104293+salma-metwally@users.noreply.github.com>
2023-06-10 07:20:26 +0900

    Update mock.ts
    update

--- mock/mock.ts
+++ mock/mock.ts
@@ -1,11 +1,11 @@
 export const data = {
-  name: "Carima Smith",
-  job: "Portrait Artist",
-  email: "virmanisatvik01@gmail.com",
-  instagram_link: "https://www.instagram.com/satvikvirmani/",
-  facebook_link: "https://www.facebook.com/satvik.virmani.9",
+  name: "Salma Metwally",
+  job: "Artist",
+  email: "salmametwally2000@gmail.com",
+  instagram_link: "https://www.instagram.com/salmametwally/",
+  facebook_link: "https://www.facebook.com/salma.osama.3158",
   aboutme_para1:
-    "Carima Smith began their professional artistic journey in Fine Arts from the Columbia Collage of Arts where I earned my qualification in Super Professional Diploma. Being a literature student helps a lot in defining art. Art is similar to poetry. Painting and poetry are similar in the way that they make “absent things present",
+    "Salma Metwally began her professional artistic journey in Fine Arts from the Education Art Helwan University where I earned my qualification in Super Professional Diploma. Being an Artistic student helps a lot in defining art. Art is similar to poetry. Painting and poetry are similar in the way that they make “absent things present",
   aboutme_para2:
-    "Unlike many of their contemporaries, Carina Smith is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
+    "Unlike many of their contemporaries, Salma Metwally is primarily self-taught and has their own approach to - and definition of - art. They believe in constantly taking risks and believe that inspiration can be found wherever they look. To find out more about their philosophy, feel free to get in touch.",
 };
